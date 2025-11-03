91e45189f503aacafeeb07bf8b7804cbfe1a5e8f
Олег Леонов <oleg@snoonu.com>
2025-11-03 20:13:29 +0700

    Fixed ISLockMessage

--- dashkit/src/main/kotlin/io/horizontalsystems/dashkit/instantsend/InstantSendLockValidator.kt
+++ dashkit/src/main/kotlin/io/horizontalsystems/dashkit/instantsend/InstantSendLockValidator.kt
@@ -14,22 +14,22 @@ class InstantSendLockValidator(
 
     @Throws
     fun validate(islock: ISLockMessage) {
-        val deterministic = islock.cycleHash != null
+        // 01. Select quorum
         val quorum = quorumListManager.getQuorum(QuorumType.LLMQ_50_60, islock.requestId)
 
         // 02. Make signId data to verify signature
+        // According to DIP-0007 and DIP-0022: SignID = SHA256(quorumHash, requestId, txHash)
+        // See: https://github.com/dashpay/dips/blob/master/dip-0007.md
+        //      https://github.com/dashpay/dips/blob/master/dip-0022.md
         val signIdPayload = BitcoinOutput()
-            .writeByte(quorum.type)
-            .write(quorum.quorumHash)
-            .write(islock.requestId)
-            .write(islock.txHash)
+                .write(quorum.quorumHash)
+                .write(islock.requestId)
+                .write(islock.txHash)
+                .toByteArray()
 
-        if (deterministic) {
-            signIdPayload.write(checkNotNull(islock.cycleHash))
-        }
+        val signId = HashUtils.doubleSha256(signIdPayload)
 
-        val signId = HashUtils.doubleSha256(signIdPayload.toByteArray())
-
+        // 03. Verify signature by BLS
         if (!bls.verifySignature(quorum.quorumPublicKey, islock.sign, signId)) {
             throw DashKitErrors.ISLockValidation.SignatureNotValid()
         }
